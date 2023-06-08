5e0407db0da4c567584e71716724903f126c9d85
yambal <jy@yambal.net>
2023-06-08 21:29:06 +0900

    xstyled

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,7 +1,18 @@
 import React from "react"
+import {
+  ThemeProvider,
+  Preflight,
+} from '@xstyled/styled-components'
+import {
+  theme
+} from './src/theme'
+
 
 export const wrapRootElement = ({ element }) => {
   return (
-    <div className="hoge">{element}</div>
+    <ThemeProvider theme={theme}>
+      <Preflight />
+      {element}
+    </ThemeProvider>
   )
 }
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import {
+  defaultTheme,
+} from '@xstyled/styled-components'
+
+export const theme = {
+  ...defaultTheme,
+  // Customize your theme here
+}
