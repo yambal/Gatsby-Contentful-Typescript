ca69006c228dc2da6291c8c6d4964c893dcc1cde
yambal <jy@yambal.net>
2023-06-08 23:39:39 +0900

    remove color mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,23 @@
 import * as React from "react"
-import { HeadFC, PageProps, graphql } from "gatsby"
-import styled from "styled-components"
+import { PageProps, graphql } from "gatsby"
 import useSiteMetadata from "../hooks/useSiteMetadata"
 import { x } from "@xstyled/styled-components"
-import { useColorMode } from '@xstyled/styled-components'
 
 const IndexPage = (props:PageProps<Queries.IndexPageQuery>) => {
   const { title, siteUrl } = useSiteMetadata()
   const { allContentfulBlog } = props.data
   const { edges } = allContentfulBlog
-  const [colorMode, setColorMode] = useColorMode()
-
-  const handleColor = React.useCallback(() => {
-    setColorMode(colorMode === 'default' ? 'dark' : 'default')
-  }, [colorMode])
 
   return (
     <x.div>
       <x.h1>{title}</x.h1>
       {edges.map((edge, i) => {
-        return (
+        return (  
           <div key={i}>
             {edge.node.title}
           </div>
         )
       })}
-      <x.button onClick={handleColor}>Color</x.button>
     </x.div>
   )
 }
